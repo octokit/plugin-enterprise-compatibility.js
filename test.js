5e68c888270e242ed22a7db77d59e3a71b7262d6
Gregor <gregor@martynus.net>
2019-01-27 08:42:42 -0800

    test: `.issues.replaceLabels()` for GHE <= 2.15

--- test.js
+++ test.js
@@ -32,3 +32,27 @@ test('octokit.issues.addLabels() sends labels in request body', t => {
     octokitPatched.issues.addLabels(options)
   ])
 })
+
+test('octokit.issues.replaceLabels() sends labels in request body', t => {
+  const octokitOriginal = Octokit({ baseUrl: 'https://original.test' })
+  const octokitPatched = Octokit.plugin(require('.'))({ baseUrl: 'https://patched.test' })
+
+  nock('https://original.test')
+    .put('/repos/octokit/rest.js/issues/1/labels', { labels: [ 'foo', 'bar' ] })
+    .reply(200, {})
+  nock('https://patched.test')
+    .put('/repos/octokit/rest.js/issues/1/labels', [ 'foo', 'bar' ])
+    .reply(200, {})
+
+  const options = {
+    owner: 'octokit',
+    repo: 'rest.js',
+    number: 1,
+    labels: ['foo', 'bar']
+  }
+
+  return Promise.all([
+    octokitOriginal.issues.replaceLabels(options),
+    octokitPatched.issues.replaceLabels(options)
+  ])
+})
