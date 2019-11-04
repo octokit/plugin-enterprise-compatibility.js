4fd6e3fee8e2bf281ecde17cc7e223a486040110
Gregor Martynus <gregor@martynus.net>
2019-11-04 14:32:27 -0800

    refactor: use `.hook.wrap` instead of `.hook.before`

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ import { VERSION } from "./version";
 import { isIssueLabelsUpdateOrReplace } from "./is-issue-labels-update-or-replace";
 
 export function enterpriseCompatibility(octokit: Octokit) {
-  octokit.hook.before("request", (options: Required<EndpointOptions>) => {
-    // see https://github.com/octokit/rest.js/blob/15.x/lib/routes.json#L3046-L3068
-    if (isIssueLabelsUpdateOrReplace(options)) {
-      options.data = options.labels;
-      delete options.labels;
+  octokit.hook.wrap(
+    "request",
+    (request, options: Required<EndpointOptions>) => {
+      // see https://github.com/octokit/rest.js/blob/15.x/lib/routes.json#L3046-L3068
+      if (isIssueLabelsUpdateOrReplace(options)) {
+        options.data = options.labels;
+        delete options.labels;
 
-      // for @octokit/rest v16.x, remove validation of labels option
-      if (options.request.validate) {
-        delete options.request.validate.labels;
+        // for @octokit/rest v16.x, remove validation of labels option
+        if (options.request.validate) {
+          delete options.request.validate.labels;
+        }
+        return request(options);
       }
     }
-  });
+  );
 }
 
 enterpriseCompatibility.VERSION = VERSION;
